576f6e13cbca1668b8f2d21a4c0b4ad90d4a457b
= <dhwngjs01@naver.com>
2023-04-10 11:42:27 +0900

    20230410_Intent

---
+++ 20230410_Anroid_Intent/app/src/main/java/com/example/a20230410_anroid_intent/CowActivity.java
@@ -0,0 +1,34 @@
+package com.example.a20230410_anroid_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CowActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnOK;
+    private EditText editSound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cow);
+
+        btnOK = (Button) findViewById(R.id.btnOK);
+        btnOK.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        if(v == btnOK){
+            Intent CallIntent = getIntent();
+            editSound = (EditText) findViewById(R.id.editInputSound);
+            CallIntent.putExtra("Animal_Sound", editSound.getText().toString());
+            setResult(RESULT_OK, CallIntent);
+            finish();
+        }
+    }
+}
